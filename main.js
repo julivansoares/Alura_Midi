fc0c68421731167e435b65eb92b68fe809c3a00a
julivansoares <julivan.soares@outlook.com>
2023-05-31 20:48:34 -0300

    modificação do main

--- main.js
+++ main.js
@@ -9,19 +9,16 @@ const listaDeAudio = document.querySelectorAll('audio');
 
 //console.log(listaDeTeclas)
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador=0;contador < listaDeTeclas.length;contador++) {
     
     const tecla=listaDeTeclas[contador];
 
     const instrumento =tecla.classList[1];
     
     const IdAudio =`#som_${instrumento}`
-    
+
     tecla.onclick= function(){
         tocaSom(IdAudio);
     }
 
-    contador = contador+1;
 }
