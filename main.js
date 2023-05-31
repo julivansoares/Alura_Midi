5607896b2136328f7c0fc79208ce712a55a4befb
julivansoares <julivan.soares@outlook.com>
2023-05-30 22:01:07 -0300

    atualização do arquivo main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+
+function tocaSomPom(){
+     document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+const listaDeAudio = document.querySelectorAll('audio');
+
+//console.log(listaDeTeclas)
+
+let contador=0;
+
+while (contador< 9) {
+
+    listaDeTeclas[0]
+    contador=contador+1
+    console.log(contador)
+}
+
+
+
+console.log(listaDeTeclas.length)
